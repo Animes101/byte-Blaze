c0954419620f81055bc4bd81a2343f4b7bdea52a
animes-barman <animesbarman101@gmail.com>
2024-03-24 12:01:59 +0600

    add piner

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Markdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
+import { useLoaderData } from 'react-router-dom'
 
 const Content = () => {
+    const content=useLoaderData();
+    const {title,body_html}=content;
+    
   return (
     <div>
-      heloo content
+      <h1>{title}</h1>
+      <div className='p-4 text-wrap	'>
+      <Markdown rehypePlugins={[rehypeRaw]}>{body_html}</Markdown>
+      </div>
     </div>
   )
 }
---
+++ src/components/LoaderSpiner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CircleLoader from "react-spinners/ClipLoader";
+
+const LoaderSpiner = () => {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+
+        <CircleLoader color="#36d7b7" />
+      
+    </div>
+  )
+}
+
+export default LoaderSpiner
